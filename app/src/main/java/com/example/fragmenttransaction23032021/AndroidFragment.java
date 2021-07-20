988cada4c018437ffe6276800e39d297892c277c
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-07-20 20:47:11 +0700

    detach va attach

--- app/src/main/java/com/example/fragmenttransaction23032021/AndroidFragment.java
+++ app/src/main/java/com/example/fragmenttransaction23032021/AndroidFragment.java
@@ -2,8 +2,10 @@ package com.example.fragmenttransaction23032021;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +13,35 @@ import android.view.ViewGroup;
 
 public class AndroidFragment extends Fragment {
 
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("BBB","onCreate Android Fragment");
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d("BBB","onCreate View Android Fragment");
         return inflater.inflate(R.layout.fragment_android, container, false);
+    }
+
+    @Override
+    public void onDestroyView() {
+        Log.d("BBB","onDestroyView");
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("BBB","onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d("BBB","onDetach");
     }
 }
--- app/src/main/java/com/example/fragmenttransaction23032021/MainActivity.java
+++ app/src/main/java/com/example/fragmenttransaction23032021/MainActivity.java
@@ -22,28 +22,72 @@ public class MainActivity extends AppCompatActivity {
     public void addAndroid(View view) {
         FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
         AndroidFragment androidFragment = new AndroidFragment();
-        fragmentTransaction.add(R.id.linearlayoutContainer,androidFragment);
+        fragmentTransaction.add(R.id.linearlayoutContainer,androidFragment ,"fragmentAndroid");
+        fragmentTransaction.addToBackStack("stackandroid");
         fragmentTransaction.commit();
     }
 
     public void addIos(View view) {
         FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
         IosFragment iosFragment = new IosFragment();
-        fragmentTransaction.add(R.id.linearlayoutContainer,iosFragment);
+        fragmentTransaction.add(R.id.linearlayoutContainer,iosFragment,"fragmentIos");
+        fragmentTransaction.addToBackStack("stackios");
         fragmentTransaction.commit();
     }
 
     public void replaceAndroid(View view) {
         FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
         AndroidFragment androidFragment = new AndroidFragment();
-        fragmentTransaction.replace(R.id.linearlayoutContainer,androidFragment);
+        fragmentTransaction.replace(R.id.linearlayoutContainer,androidFragment,"fragmentAndroid");
+        fragmentTransaction.addToBackStack("stackandroid");
         fragmentTransaction.commit();
     }
 
     public void replaceIos(View view) {
         FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
         IosFragment iosFragment = new IosFragment();
-        fragmentTransaction.replace(R.id.linearlayoutContainer,iosFragment);
+        fragmentTransaction.replace(R.id.linearlayoutContainer,iosFragment,"fragmentIos");
+        fragmentTransaction.addToBackStack("stackios");
         fragmentTransaction.commit();
+    }
+
+    public void removeAndroid(View view) {
+        AndroidFragment androidFragment = (AndroidFragment) mFragmentManager.findFragmentByTag("fragmentAndroid");
+        if (androidFragment != null){
+            FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+            fragmentTransaction.remove(androidFragment);
+            fragmentTransaction.commit();
+        }
+    }
+
+    public void removeIos(View view) {
+        IosFragment iosFragment = (IosFragment) mFragmentManager.findFragmentByTag("fragmentIos");
+        if (iosFragment != null){
+            FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+            fragmentTransaction.remove(iosFragment);
+            fragmentTransaction.commit();
+        }
+    }
+
+    public void popbackstack(View view) {
+        mFragmentManager.popBackStack(2,FragmentManager.POP_BACK_STACK_INCLUSIVE);
+    }
+
+    public void detach(View view) {
+        AndroidFragment androidFragment = (AndroidFragment) mFragmentManager.findFragmentByTag("fragmentAndroid");
+        if (androidFragment != null){
+            FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+            fragmentTransaction.detach(androidFragment);
+            fragmentTransaction.commit();
+        }
+    }
+
+    public void attach(View view) {
+        AndroidFragment androidFragment = (AndroidFragment) mFragmentManager.findFragmentByTag("fragmentAndroid");
+        if (androidFragment != null){
+            FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+            fragmentTransaction.attach(androidFragment);
+            fragmentTransaction.commit();
+        }
     }
 }
